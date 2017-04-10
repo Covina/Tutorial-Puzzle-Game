c57c8fb7158d190da6089ae196e173a24e1e515d
Covina <jabs303@gmail.com>
2017-04-09 21:25:53 -0700

    Section 7, Lecture 113;  Completed and Wired up the settings Slide In/Out animation

---
+++ Assets/_Scenes/Scripts/1 - Puzzle Menu Controller Scripts/SelectPuzzle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPuzzle : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject selectPuzzleMenuPanel;
+
+	[SerializeField]
+	private Animator selectPuzzleMenuAnimator;
+
+
+	private string selectedPuzzle;
+
+
+
+	public void SelectedPuzzle() 
+	{
+		selectedPuzzle = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+		Debug.Log(selectedPuzzle);
+		
+
+
+	}
+
+
+}
---
+++ Assets/_Scenes/Scripts/1 - Puzzle Menu Controller Scripts/SettingsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsController : MonoBehaviour {
+
+
+	[SerializeField] private GameObject settingsPanel;
+
+	[SerializeField] private Animator animator;
+
+
+
+	public void OpenSettingsPanel()
+	{
+		// Activate the settings menu
+		settingsPanel.SetActive(true);
+
+		// play the fade-in animation
+		animator.Play("Settings - Slide In");
+
+	}
+
+	public void CloseSettingsPanel()
+	{
+		StartCoroutine( CloseSettings() );
+	}
+
+
+	IEnumerator CloseSettings ()
+	{
+		// play the slide out animation
+		animator.Play("Settings - Slide Out");
+
+		// pause for a second to let the animation play
+		yield return new WaitForSeconds(1.0f);
+
+		// deactivate the panel
+		settingsPanel.SetActive(false);
+
+
+	}
+
+
+
+
+}
