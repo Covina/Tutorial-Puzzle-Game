d2b308f0414dab133167086da98cc18e97996726
Covina <jabs303@gmail.com>
2017-04-11 23:35:43 -0700

    Section 7, Lecture 126;  Now checking if Guesses match and are correct; Enforcing 2 guess limit;

--- Assets/Scripts/3 - Puzzle Game Controller/PuzzleGameManager.cs
+++ Assets/Scripts/3 - Puzzle Game Controller/PuzzleGameManager.cs
@@ -15,11 +15,102 @@ public class PuzzleGameManager : MonoBehaviour {
 	private int level;
 	private string selectedPuzzle;
 
+	private Sprite puzzleBackgroundImage;
+
+	private bool firstGuess, secondGuess;
+	private int firstGuessIndex, secondGuessIndex;
+	private string firstGuessImageName, secondGuessImageName;
+
 
 	public void PickAPuzzle ()
 	{
-		Debug.Log("The selected button is " + UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+		if (!firstGuess) {
+			firstGuess = true;
+
+			firstGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+			// get the name of the first guess
+			firstGuessImageName = gamePuzzleSprites[firstGuessIndex].name;
+
+			StartCoroutine( TurnPuzzleButtonUp (puzzleButtonsAnimators[firstGuessIndex], puzzleButtons[firstGuessIndex], gamePuzzleSprites[firstGuessIndex]) );
+
+		} else if (!secondGuess) {
+			secondGuess = true;
+
+			secondGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+			// get the name of the second guess
+			secondGuessImageName = gamePuzzleSprites[secondGuessIndex].name;
+
+			StartCoroutine( TurnPuzzleButtonUp (puzzleButtonsAnimators[secondGuessIndex], puzzleButtons[secondGuessIndex], gamePuzzleSprites[secondGuessIndex]) );
+		
+
+		}
+
+		// check if the two guesses match
+		StartCoroutine( CheckIfGuessesMatch (puzzleBackgroundImage) );
+
 	}
+
+	IEnumerator CheckIfGuessesMatch (Sprite bgimage)
+	{
+		yield return new WaitForSeconds (1.7f);
+
+		if (firstGuessImageName == secondGuessImageName) {
+			// correct guess
+
+			puzzleButtonsAnimators [firstGuessIndex].Play ("FadeOut");
+			puzzleButtonsAnimators [secondGuessIndex].Play ("FadeOut");
+
+			// TODO - increment score
+		} else {
+			// incorrect guess
+
+			// flip first one back
+			StartCoroutine( TurnPuzzleButtonBack (puzzleButtonsAnimators[firstGuessIndex], puzzleButtons[firstGuessIndex], bgimage) );
+
+			// flip second one back
+			StartCoroutine( TurnPuzzleButtonBack (puzzleButtonsAnimators[secondGuessIndex], puzzleButtons[secondGuessIndex], bgimage) );
+
+		}
+
+		// wait some 
+		yield return new WaitForSeconds(.7f);
+
+		// reset our guess bools
+		firstGuess = false;
+		secondGuess = false;
+
+
+	}
+
+
+
+	IEnumerator TurnPuzzleButtonUp (Animator anim, Button btn, Sprite puzzleImage)
+	{
+		
+		anim.Play("TurnUp");
+
+		yield return new WaitForSeconds(0.5f);
+
+		btn.image.sprite = puzzleImage;
+
+
+	}
+
+	IEnumerator TurnPuzzleButtonBack (Animator anim, Button btn, Sprite puzzleImage)
+	{
+		
+		anim.Play("TurnBack");
+
+		yield return new WaitForSeconds(0.5f);
+
+		btn.image.sprite = puzzleImage;
+
+
+	}
+
 
 	void AddListeners ()
 	{
@@ -40,6 +131,8 @@ public class PuzzleGameManager : MonoBehaviour {
 	{
 		this.puzzleButtons = buttons;
 		this.puzzleButtonsAnimators = animators;
+
+		puzzleBackgroundImage = puzzleButtons[0].image.sprite;
 
 		AddListeners();
 	}
